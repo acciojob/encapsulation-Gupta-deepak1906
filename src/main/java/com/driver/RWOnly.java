117d010e4a0ea4277361003274dbf46c2279c082
Gupta-deepak1906 <gdeepakg9997@gmail.com>
2024-03-14 00:23:21 +0530

    update

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+
+    //constructor
+    public RWOnly(String name)
+    {
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public String setName(String name){
+        return name;
+    }
     
 }
