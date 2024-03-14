d7ef50752cec4554a32e68117203377ee0223dbe
Gupta-deepak1906 <gdeepakg9997@gmail.com>
2024-03-14 19:51:53 +0530

    Mistake is Modify

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,8 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        RWOnly obj = new RWOnly("John");
+        RWOnly obj = new RWOnly();
+        obj.setName("John");
 
         System.out.println(obj.getName());
     }
